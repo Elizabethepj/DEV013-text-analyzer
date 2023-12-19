0bab13c9d59d1475464cd7b63994bf14be27d78f
Elizabeth <elizabethepj@gmail.com>
2023-12-19 11:13:31 -0500

    cuartaVersión

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,67 +1,65 @@
 
 const analyzer = {  
-//1. TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    getWordCount: (text) => {
-    const words = text.split (/\s+/).filter(word => word !== '');
+  // 1. TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getWordCount: (text) => {
+    const words = text.split(/\s+/).filter(word => word !== '');
     return words.length;
-    },
+  },
 
- //2. TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-   getCharacterCount: (text) => {
+  //2. TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+  getCharacterCount: (text) => {
     return text.length;
-     },
+  },
 
   //3. TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   // [^\w] excluye espacios y signos de puntuación. \s así se representa el espacio en blanco, pero no funcionó enn este código.    
   getCharacterCountExcludingSpaces: (text) => {
     let charNoSpaceCount = 0;
-     for (const char of text) {
-     if (/\w/.test(char)) {
-      charNoSpaceCount++;
+    for (const char of text) {
+      if (/\w/.test(char)) {
+        charNoSpaceCount++;
       }
     }
-      return charNoSpaceCount;
+    return charNoSpaceCount;
   },
   
-  //
   //getCharacterCountExcludingSpaces: (text)=> {
-    //const charNoSpaceCount = text.split('').reduce((count,char) => {
-    //if (/\w/.test (char)) {
-    //return count + 1;}
-    //return count;
+  //const charNoSpaceCount = text.split('').reduce((count,char) => {
+  //if (/\w/.test (char)) {
+  //return count + 1;}
+  //return count;
   //}, 0);
-   // return charNoSpaceCount;
- // },
+  // return charNoSpaceCount;
+  // },
 
-  // // replace: elimina caracteres no deseados. Este fue el primer código para contar los caracteres sin espacios. Sin embargo, este no tenía lógica boleana.
+  //replace: elimina caracteres no deseados. Este fue el primer código para contar los caracteres sin espacios. Sin embargo, este no tenía lógica boleana.
   //getCharacterCountExcludingSpaces: (text) => {
-     // const charNoSpaceCount = text.replace(/[^\w]/gi,'').length;  
-     // return charNoSpaceCount;
+  // const charNoSpaceCount = text.replace(/[^\w]/gi,'').length;  
+  // return charNoSpaceCount;
   //},
 
-   //4. TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-   getNumberCount: (text) => {
+  //4. TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  getNumberCount: (text) => {
     const numbers = text.match(/\d+/g) || [];
     return numbers.length;
-    },
+  },
 
-   //5. TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-   //En esta función se convierten cadenas tipo string a tipo number. text.match(/\d+/g) busca los números y los ubica en un array, y parseInt(num) dentro de reduce los convierte en un valor númerico. 
-   getNumberSum: (text) => {
-        const numbers = text.match(/\d+/g) || [];
-        const totalSum = numbers.reduce((sum, num) => sum + parseInt(num), 0);
-        return totalSum;
-      },
+  //5. TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+  //En esta función se convierten cadenas tipo string a tipo number. text.match(/\d+/g) busca los números y los ubica en un array, y parseInt(num) dentro de reduce los convierte en un valor númerico. 
+  getNumberSum: (text) => {
+    const numbers = text.match(/\d+/g) || [];
+    const totalSum = numbers.reduce((sum, num) => sum + parseInt(num), 0);
+    return totalSum;
+  },
 
-    //6. TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  //6. TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   // split: divide el texto en palabras basándose en espacios. Filter: elimina las palabras vacías.
   getAverageWordLength: (text) => {    
-  const words = text.split(/\s+/).filter(word => word !== '');
-  const totalWordLength = words.reduce((sum, word) => sum + word.length, 0);
-  const averageWordLength = words.length ? totalWordLength / words.length : 0.00;
-  return parseFloat(averageWordLength.toFixed(2));
+    const words = text.split(/\s+/).filter(word => word !== '');
+    const totalWordLength = words.reduce((sum, word) => sum + word.length, 0);
+    const averageWordLength = words.length ? totalWordLength / words.length : 0.00;
+    return parseFloat(averageWordLength.toFixed(2));
   },
 
 };
-
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar los métodos del objeto `analyzer`
-
 //Espera a que el DOM esté completamente cargado antes de ejecutar el código JavaScript, asegurando que todos los elementos del documento estén disponibles para ser manipulados.
 document.addEventListener('DOMContentLoaded',
-function() {
+  function() {
     const userInput = document.querySelector('textarea[name="userInput"]');
     const resetButton = document.getElementById('resetButton');
     const resultElements = {
-        wordCount: document.querySelector ('[data-testid="word-count"] span'),
-        charCount: document.querySelector ('[data-testid="character-count"] span'),
-        charNoSpaceCount: document.querySelector ('[data-testid = "character-no-spaces-count"] span'),
-        numberCount: document.querySelector ('[data-testid= "number-count"] span'),
-        numberSum: document.querySelector ('[data-testid="number-sum"] span'),
-        wordLengthAverage: document.querySelector ('[data-testid="word-length-average"] span'),
+      wordCount: document.querySelector ('[data-testid="word-count"] span'),
+      charCount: document.querySelector ('[data-testid="character-count"] span'),
+      charNoSpaceCount: document.querySelector ('[data-testid = "character-no-spaces-count"] span'),
+      numberCount: document.querySelector ('[data-testid= "number-count"] span'),
+      numberSum: document.querySelector ('[data-testid="number-sum"] span'),
+      wordLengthAverage: document.querySelector ('[data-testid="word-length-average"] span'),
     };
 
-function calcularMetricas(){
-    const text = userInput.value;
-          resultElements.wordCount.textContent = analyzer.getWordCount(text);
-          resultElements.charCount.textContent = analyzer.getCharacterCount(text);
-          resultElements.charNoSpaceCount.textContent = analyzer.getCharacterCountExcludingSpaces(text);
-          resultElements.numberCount.textContent = analyzer.getNumberCount(text);
-          resultElements.numberSum.textContent = analyzer.getNumberSum(text);
-          resultElements.wordLengthAverage.textContent = analyzer.getAverageWordLength (text);
-          }
+    function calcularMetricas(){
+      const text = userInput.value;
+      resultElements.wordCount.textContent = analyzer.getWordCount(text);
+      resultElements.charCount.textContent = analyzer.getCharacterCount(text);
+      resultElements.charNoSpaceCount.textContent = analyzer.getCharacterCountExcludingSpaces(text);
+      resultElements.numberCount.textContent = analyzer.getNumberCount(text);
+      resultElements.numberSum.textContent = analyzer.getNumberSum(text);
+      resultElements.wordLengthAverage.textContent = analyzer.getAverageWordLength (text);
+    }
 
-         userInput.addEventListener('input', calcularMetricas);
-         console.log('Limpiar');
-         resetButton.addEventListener('click', function(){
-         userInput.value = '';
-         calcularMetricas();
-          });
-
-
-});
+    userInput.addEventListener('input', calcularMetricas);
+    resetButton.addEventListener('click', function(){
+      userInput.value = '';
+      calcularMetricas();
+    });
+  });
