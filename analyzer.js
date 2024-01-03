7adb729ea5c8968d0825c40c20797e81181a9158
Elizabeth <elizabethepj@gmail.com>
2024-01-03 12:12:07 -0500

    Updates

---
+++ analyzer.js
@@ -0,0 +1,73 @@
+
+const analyzer = {
+  // 1. TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getWordCount: (text) => {
+    const words = text.split(/\s+/).filter(word => word !== '');
+    return words.length;
+  },
+
+  //2. TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+  getCharacterCount: (text) => {
+    return text.length;
+  },
+
+  //3. TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+  // [^\w] excluye espacios y signos de puntuación. \s así se representa el espacio en blanco, pero no funcionó enn este código.    
+  getCharacterCountExcludingSpaces: (text) => {
+    let charNoSpaceCount = 0;
+    for (let i = 0; i < text.length; i++) {
+      const char = text[i];
+      if (/\w/.test(char)) {
+        charNoSpaceCount++;
+      }
+    }
+    return charNoSpaceCount;
+  },
+
+  //getCharacterCountExcludingSpaces: (text)=> {
+  //const charNoSpaceCount = text.split('').reduce((count,char) => {
+  //if (/\w/.test (char)) {
+  //return count + 1;}
+  //return count;
+  //}, 0);
+  // return charNoSpaceCount;
+  // },
+
+  //replace: elimina caracteres no deseados. Este fue el primer código para contar los caracteres sin espacios. Sin embargo, este no tenía lógica boleana.
+  //getCharacterCountExcludingSpaces: (text) => {
+  // const charNoSpaceCount = text.replace(/[^\w]/gi,'').length;  
+  // return charNoSpaceCount;
+  //},
+
+  //4. TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  getNumberCount: (text) => {
+    const numbers = text.match(/[-+]?\b\d+(\.\d+)?\b/g) || [];
+    const numCount = numbers.reduce((count, num) => {
+      const numVal = parseFloat(num);
+      if (!isNaN(numVal)) {
+        return count + 1;
+      }
+      return count;
+    }, 0);
+    return numCount;
+  },
+
+  // 5. TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+  //En esta función se conviertengit cadenas tipo string a tipo number. text.match(/\d+/g) busca los números y los ubica en un array, y parseInt(num) dentro de reduce los convierte en un valor númerico. 
+  getNumberSum: (text) => {
+    const numbers = text.match(/[-+]?\b\d+(\.\d+)?\b/g) || [];
+    const totalSum = numbers.reduce((sum, num) => sum + parseFloat(num), 0);
+    return parseFloat(totalSum.toFixed(2));
+  },
+
+  //6. TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  // split: divide el texto en palabras basándose en espacios. Filter: elimina las palabras vacías.
+  getAverageWordLength: (text) => {
+    const words = text.split(/\s+/).filter(word => word !== '');
+    const totalWordLength = words.reduce((sum, word) => sum + word.length, 0);
+    const averageWordLength = words.length ? totalWordLength / words.length : 0.00;
+    return parseFloat(averageWordLength.toFixed(2));
+  },
+
+};
+export default analyzer;
---
+++ index.js
@@ -0,0 +1,53 @@
+import analyzer from './analyzer.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const userInput = document.querySelector('textarea[name="user-input"]');
+  const resetButton = document.getElementById('resetButton');
+
+  function WordCount() {
+    const text = userInput.value;
+    document.querySelector('[data-testid="word-count"]').textContent = 'Recuento de palabras: ' + analyzer.getWordCount(text);
+  }
+
+  function CharCount() {
+    const text = userInput.value;
+    document.querySelector('[data-testid="character-count"]').textContent = 'Recuento de caracteres: ' + analyzer.getCharacterCount(text);
+  }
+
+  function CharNoSpaceCount() {
+    const text = userInput.value;
+    document.querySelector('[data-testid="character-no-spaces-count"]').textContent = 'Recuento de caracteres (sin espacios y signos): ' + analyzer.getCharacterCountExcludingSpaces(text);
+  }
+
+  function NumberCount() {
+    const text = userInput.value;
+    document.querySelector('[data-testid="number-count"]').textContent = 'Recuento de números: ' + analyzer.getNumberCount(text);
+  }
+
+  function NumberSum() {
+    const text = userInput.value;
+    document.querySelector('[data-testid="number-sum"]').textContent = 'Suma total de números: ' + analyzer.getNumberSum(text);
+  }
+
+  function WordLengthAverage() {
+    const text = userInput.value;
+    document.querySelector('[data-testid="word-length-average"]').textContent = 'Longitud media de las palabras: ' + analyzer.getAverageWordLength(text);
+  }
+  
+  userInput.addEventListener('input', WordCount);
+  userInput.addEventListener('input', CharCount);
+  userInput.addEventListener('input', CharNoSpaceCount);
+  userInput.addEventListener('input', NumberCount);
+  userInput.addEventListener('input', NumberSum);
+  userInput.addEventListener('input', WordLengthAverage);
+
+  resetButton.addEventListener('click', function () {
+    userInput.value = '';
+    WordCount();
+    CharCount();
+    CharNoSpaceCount();
+    NumberCount();
+    NumberSum();
+    WordLengthAverage();
+  });
+});
